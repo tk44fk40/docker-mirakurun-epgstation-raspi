9584aa2403aa94456e8080adf7d00b4dd8f3dce6
Takayoshi Fukushima <tk44fk40@gmail.com>
2022-09-10 20:27:46 +0900

    ffmpegのオプション解説を追加

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-git clone https://github.com/l3tnun/docker-mirakurun-epgstation.git
-cd docker-mirakurun-epgstation
+git clone https://github.com/tk44fk40/docker-mirakurun-epgstation-raspi.git
+cd docker-mirakurun-epgstation-raspi
 cp docker-compose-sample.yml docker-compose.yml
 cp epgstation/config/enc.js.template epgstation/config/enc.js
+cp epgstation/config/enc_rpi.js.template epgstation/config/enc_rpi.js
 cp epgstation/config/config.yml.template epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
