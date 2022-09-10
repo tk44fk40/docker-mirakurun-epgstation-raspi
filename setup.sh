2fb291c83f2d0b47920f40d1d026f42e521bf467
Takayoshi Fukushima <tk44fk40@gmail.com>
2022-09-10 23:35:59 +0900

    docker-compose コマンドの実行で sudo するよう修正

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ cp epgstation/config/config.template.yml epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
 cp epgstation/config/serviceLogConfig.sample.yml epgstation/config/serviceLogConfig.yml
-docker-compose run --rm -e SETUP=true mirakurun
+sudo docker-compose run --rm -e SETUP=true mirakurun
