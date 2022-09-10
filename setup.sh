ffa914157989a7246ec43dc64a37072f0df74e12
Takayoshi Fukushima <tk44fk40@gmail.com>
2022-09-10 21:04:05 +0900

    テンプレートのファイル名変更を反映

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 git clone https://github.com/tk44fk40/docker-mirakurun-epgstation-raspi.git
 cd docker-mirakurun-epgstation-raspi
 cp docker-compose-sample.yml docker-compose.yml
-cp epgstation/config/enc.js.template epgstation/config/enc.js
-cp epgstation/config/enc_rpi.js.template epgstation/config/enc_rpi.js
-cp epgstation/config/config.yml.template epgstation/config/config.yml
+cp epgstation/config/enc.template.js epgstation/config/enc.js
+cp epgstation/config/enc_rpi.template.js epgstation/config/enc_rpi.js
+cp epgstation/config/config.template.yml epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
 cp epgstation/config/serviceLogConfig.sample.yml epgstation/config/serviceLogConfig.yml
