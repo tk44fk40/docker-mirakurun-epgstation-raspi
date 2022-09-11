00fafe1012b71957a3f9bc193565c37cb6bd3fe2
Takayoshi Fukushima <tk44fk40@gmail.com>
2022-09-11 19:24:45 +0900

    config.yml.template のリネームを revert.（EPGStation が当該ファイルを参照している）

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd docker-mirakurun-epgstation-raspi
 cp docker-compose-sample.yml docker-compose.yml
 cp epgstation/config/enc.template.js epgstation/config/enc.js
 cp epgstation/config/enc_rpi.template.js epgstation/config/enc_rpi.js
-cp epgstation/config/config.template.yml epgstation/config/config.yml
+cp epgstation/config/config.yml.template epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
 cp epgstation/config/serviceLogConfig.sample.yml epgstation/config/serviceLogConfig.yml
